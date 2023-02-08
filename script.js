7cadc53b08e96f121d0428989c60d1446bb919ae
Madina <madinatussupova0820@gmail.com>
2023-02-08 14:11:23 +0600

    set background and greeting dynamically

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const time = document.querySelector("#time");
-const greeting = document.querySelector("#time");
+const greeting = document.querySelector("#greeting");
 const name = document.querySelector("#name");
 const focus = document.querySelector("#focus");
 
@@ -30,9 +30,47 @@ function addZero(n) {
 }
 
 // Set background and greeting
-function set() {
+function setBgAndGreet() {
+  let currentHour = new Date().getHours();
+  if(currentHour < 12) {
+    document.body.style.backgroundImage = "url('imgs/morning.jpg')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+    greeting.textContent = 'Good morning,'
+  } else if(currentHour < 18) {
+    document.body.style.backgroundImage = "url('imgs/afternoon.jpg')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundCover = 'cover';
+    greeting.textContent = 'Good afternoon,'
+  } else {
+    document.body.style.backgroundImage = "url('imgs/night.jpg')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = 'cover';
+    greeting.textContent = 'Good evening,'
+    document.body.style.color = 'white';
+  }
+}
 
+// Get Name
+function getName() {
+  if(localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Your Name]';
+  } else {
+    localStorage.getItem('name');
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if(localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Your Focus]';
+  } else {
+    localStorage.getItem('focus');
+  }
 }
 
 // Run
 showTime();
+setBgAndGreet();
+getName();
+getFocus();
